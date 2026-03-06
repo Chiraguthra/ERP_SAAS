2a803a16dd780913a9abafe1bd5409cf350a9316
chirag <chiraguthra17@gmail.com>
2026-03-06 23:22:19 +0530

    temp gignor change

---
+++ src/android/app/build/generated/source/buildConfig/debug/com/erp/webview/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.erp.webview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.erp.webview";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from default config.
+  public static final String ERP_BOOTSTRAP_URL = "http://103.127.30.237/api/mobile/bootstrap";
+  // Field from default config.
+  public static final String ERP_BOOTSTRAP_USERNAME = "admin";
+  // Field from default config.
+  public static final String ERP_GUEST_TOKEN_URL = "http://103.127.30.237/api/mobile/guest-token";
+  // Field from default config.
+  public static final String ERP_MOBILE_SECRET = "";
+  // Field from default config.
+  public static final boolean ERP_USE_GUEST_MODE = true;
+  // Field from default config.
+  public static final String ERP_WEB_URL = "http://103.127.30.237";
+}
